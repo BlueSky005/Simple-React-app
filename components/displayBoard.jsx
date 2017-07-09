904ae9eabbc7d75eb75f9a006d65d72bad85d2bd
madhu <madhu@gmail.com>
2017-07-09 18:07:03 +0530

    Working after taking help

--- components/displayBoard.jsx
+++ components/displayBoard.jsx
@@ -5,24 +5,20 @@ import _ from 'lodash';
 
 export default class DisplayBoard extends Component  {
 
-
 componentWillMount() {
-
-  this.setState({ products:this.props.products  });
-  this.setState({ productNames: _.map(this.props.products, 'product_name')  });
+this.setState({ products:this.props.products  });
+this.setState({ productNames: _.map(this.props.products, 'product_name')  });
   this.handleInputChange = this.handleInputChange.bind(this);
-
 }
 
 handleInputChange(e) {
   let selectedProduct = e.target.value;
 
-  let filteredProducts = _.filter(this.state.products,['product_name',selectedProduct]);
+  let filteredProducts = _.filter(this.props.products,['product_name',selectedProduct]);
 
   console.log(filteredProducts);
 
-  // Not working
-  //this.setState({products: {filteredProducts}});
+  this.setState({products: filteredProducts});
 }
 
 loadProducts()  {
@@ -30,14 +26,18 @@ loadProducts()  {
   let productNames = _.uniqWith(this.state.productNames, _.isEqual);
 
   return productNames.map((productName) => {
-    return (  <div><input type="radio" name="names" value={productName} key={productName} onChange={this.handleInputChange} />{productName}<br /></div> );
+    return (
+              <div>
+                <input type="radio" name="names" value={productName} key={productName} onChange={this.handleInputChange} />{productName}<br />
+              </div>
+           );
   });
 
 }
 
 
 LoadItems() {
-  return this.props.products.map((product) => {
+  return this.state.products.map((product) => {
     return (
 
           <tr key={product.productId}>
